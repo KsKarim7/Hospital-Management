3edb4d243b217ca3018fa7e16e27e389ed6ec793
Khaled Karim <kskarim177@gmail.com>
2024-10-10 23:49:20 +0600

    some mods

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const loadServices = () => {
   fetch("https://testing-8az5.onrender.com/services/")
-    .then(res => res.json())
-    .then(data => displayServices(data))
-    .catch(error => console.log(error));
-}
+    .then((res) => res.json())
+    .then((data) => displayService(data))
+    .catch((err) => console.log(err));
+};
 
-const displayServices = (services) => {
+const displayService = (services) => {
+  //   console.log(services);
   services.forEach((service) => {
     const parent = document.getElementById("service-container");
     const li = document.createElement("li");
     li.innerHTML = `
-        <div class="card shadow h-100">
+      <div class="card shadow h-100">
                 <div class="ratio ratio-16x9">
                   <img
                     src=${service.image}
@@ -22,7 +23,7 @@ const displayServices = (services) => {
                 <div class="card-body p-3 p-xl-5">
                   <h3 class="card-title h5">${service.name}</h3>
                   <p class="card-text">
-                    ${service.description.slice(0, 160)}
+                    ${service.description.slice(0, 140)}
                   </p>
                   <a href="#" class="btn btn-primary">Details</a>
                 </div>
