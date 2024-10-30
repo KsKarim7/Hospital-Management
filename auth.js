e8e3a3adf89c7c770566f7e2753771a3d7f5493c
Khaled Karim <kskarim177@gmail.com>
2024-10-31 03:38:57 +0600

    User Login

--- auth.js
+++ auth.js
@@ -33,16 +33,40 @@ const handleRegistration = (event) => {
                 .then((data) => console.log(data));
         } else {
             document.getElementById("error").innerText =
-                "pass must contain eight characters, at least one letter, one number and one special character:";
+                "Password must contain eight characters, at least one letter, one number and one special character:";
         }
     } else {
         document.getElementById("error").innerText =
             "password and confirm password do not match";
-        alert("password and confirm password do not match");
+        alert("Passwords doesn't match! Enter Again.");
     }
 };
 
 const getValue = (id) => {
     const value = document.getElementById(id).value;
     return value;
+};
+
+const handleLogin = (event) => {
+    event.preventDefault();
+    const username = getValue("login-username");
+    const password = getValue("login-password");
+    console.log(username, password);
+    if ((username, password)) {
+        fetch("https://testing-8az5.onrender.com/patient/login/", {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify({ username, password }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+
+                if (data.token && data.user_id) {
+                    localStorage.setItem("token", data.token);
+                    localStorage.setItem("user_id", data.user_id);
+                    window.location.href = "index.html";
+                }
+            });
+    }
 };
